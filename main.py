985698675c95a34bf3fa0041b10dc19daba14e98
Nischitha <nischithar36@gmail.com>
2024-06-20 21:50:34 +0530

    Built a REST Api.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home/")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature =39
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 app.run(debug=True)
